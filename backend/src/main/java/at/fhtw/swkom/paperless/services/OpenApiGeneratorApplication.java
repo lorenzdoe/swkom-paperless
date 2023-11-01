7670932feef54d56b40f385b85aa471dde2819a5
Lorenz Doellinger <if21b084@technikum-wien.at>
2023-11-01 19:55:17 +0100

    changed in application.properties spring.jpa.hibernate.ddl-auto=update to automatically create missing tables and columns

--- backend/src/main/java/at/fhtw/swkom/paperless/services/OpenApiGeneratorApplication.java
+++ backend/src/main/java/at/fhtw/swkom/paperless/services/OpenApiGeneratorApplication.java
@@ -5,6 +5,7 @@ import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -17,6 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 @EnableJpaRepositories(
     basePackages = {"at.fhtw.swkom.paperless.persistance.repositories"}
+)
+@EntityScan(
+    basePackages = {"at.fhtw.swkom.paperless.persistance.entities"}
 )
 public class OpenApiGeneratorApplication {
 
