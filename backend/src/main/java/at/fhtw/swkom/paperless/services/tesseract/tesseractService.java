e972b750505d5b167a2b8542c3bbf495effdf5bb
Amelie1987 <amelie.boehme@gmail.com>
2023-11-24 10:49:31 +0100

    added tesseract in docker container "ocrworker"

---
+++ backend/src/main/java/at/fhtw/swkom/paperless/services/tesseract/tesseractService.java
@@ -0,0 +1,17 @@
+package at.fhtw.swkom.paperless.services.tesseract;
+
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class tesseractService {
+
+
+        Tesseract tesseract = new Tesseract();
+        //tesseract.setDatapath("/backend/services/tesseract");
+        //tesseract.setLanguage("eng");
+        //tesseract.setDatapath("./");
+            // test
+
+    }
+
