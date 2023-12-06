cf2760ff6b71187db09f6ad09ac33530f6d05777
Lorenz Doellinger <if21b084@technikum-wien.at>
2023-12-06 17:49:13 +0100

    orc worker finished, backend not working

--- ocrService/src/main/java/at/fhtw/persistance/MinIORepository.java
+++ ocrService/src/main/java/at/fhtw/persistance/MinIORepository.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.io.InputStream;
 
