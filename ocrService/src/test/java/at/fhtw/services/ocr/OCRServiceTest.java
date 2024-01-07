35aa2fc2ff1e5a2727c614a57092d6b92d912889
Lorenz Doellinger <if21b084@technikum-wien.at>
2024-01-07 17:29:26 +0100

    ocr tests

--- ocrService/src/test/java/at/fhtw/services/ocr/OCRServiceTest.java
+++ ocrService/src/test/java/at/fhtw/services/ocr/OCRServiceTest.java
@@ -5,20 +5,38 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    * This class is used to test the OCRService class.
+    * It uses a test.pdf file to test the OCR functionality.
+    * It needs a valid Tesseract installation to work
+ */
 
 @SpringBootTest
 public class OCRServiceTest {
 
-    @Autowired
     private OCRService ocrService;
 
     @Test
-    public void testOCR() {
+    public void testOCR() throws IOException {
         // Arrange
+        ocrService = new OCRService();
         String filePath = "src/test/java/at/fhtw/services/ocr/test.pdf";
-        MockMultipartFile file = new MockMultipartFile("test.pdf", filePath, "application/pdf", "test".getBytes());
+        FileInputStream input = new FileInputStream(filePath);
+        MultipartFile file = new MockMultipartFile("test", "test.pdf", "application/pdf", input);
 
+        // Act
+        String result = ocrService.performOcr(file);
+
+        // Assert
+        assertNotNull(result);
+        assertTrue(result.contains("This is a test"));
     }
 }
